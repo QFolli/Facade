cbd0de58e08badfc721ead8296bfe1fbcb8e5a3e
Виталий Моисеенко <88559269+QFolli@users.noreply.github.com>
2022-01-21 11:38:24 +0200

    Add files via upload

---
+++ Facade/Facade/Tuner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Tuner
+    {
+        public void On()
+        {
+            Console.WriteLine("Тюнер включён");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Тюнер выключен");
+        }
+
+        public void SetAm()
+        {
+            Console.WriteLine("AM установлен");
+        }
+
+        public void SetFm()
+        {
+            Console.WriteLine("Fm установлен");
+        }
+
+        public void SetFrequency(int frequency)
+        {
+            Console.WriteLine($"Выставлена частота {frequency}.");
+        }
+    }
+}
